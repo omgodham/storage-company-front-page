90ebf062c2ba6a7603bd6954aae495fba8a484a2
abc <abc@example.com>
2020-12-19 12:15:44 +0530

    Contact section finished

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,8 +1,17 @@
 import "./Contact.css";
+import RoomIcon from '@material-ui/icons/Room';
+import MailIcon from '@material-ui/icons/Mail';
+import PhoneIcon from '@material-ui/icons/Phone';
 function Contact(){
         return <div className="contact">
                   <h3 >CONTACT</h3>
         <p >Lets get in touch. Send us a message:</p>
+        <div className="text-section">
+        <h4><RoomIcon  className="contact-icons"/>Chicago,US</h4>
+        <h4><PhoneIcon className="contact-icons" />Phone: +00 151515</h4>
+        <h4><MailIcon  className="contact-icons"/>Email: mail@mail.com</h4>
+        </div>
+        <img src="https://www.w3schools.com/w3images/map.jpg" /> 
         </div>
 
 }
---
+++ src/components/Footer.js
@@ -0,0 +1,9 @@
+import "./Footer.css";
+import ArrowUpwardIcon from '@material-ui/icons/ArrowUpward';
+
+function  Footer() {
+    return <div className="footer">
+            <button><span><ArrowUpwardIcon/></span> To the Top</button>
+    </div>
+}
+export default Footer;
