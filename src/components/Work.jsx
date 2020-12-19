eba7d866f18b74633c76c02370335ec4a2f9d13b
abc <abc@example.com>
2020-12-19 20:58:51 +0530

    Final Version with mobile responsiveness

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -3,9 +3,11 @@ import React from "react";
 import images from "./images";
 function Work() {
 function  show(item) {
-    return <img className="image" src={item}></img>;
+    return <div className="image-div">
+    <img className="image" src={item}></img>
+    </div>;
 }
-    return <div id="Work" className="team">
+    return <div id="work" className="team">
     <h3 className="heading" style={{paddingTop:0}}>OUR WORK</h3>
     <p className="heading2">What we've done for people</p>
     <div className="images">
