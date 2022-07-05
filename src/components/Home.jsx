72e4f96dd8ffa7d8659ea20ac615ff75880b0ecd
omkargodham <omkargodham321@gmail.com>
2022-07-05 17:28:12 +0530

    small changes

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,25 +1,26 @@
-import "./Home.css"
+import "./Home.css";
 function Home() {
-  return        <div class="home-page" id="home">
-  <div class="content">
-      <h1 class="heading">Start something that matters</h1>
-      <p class="description">
+  return (
+    <div class="home-page" id="home">
+      <div class="content">
+        <h1 class="heading">Start something that matters</h1>
+        <p class="description">
           Stop wasting valuable time with projects that just isn't you.
-      </p>
-      <button class="home-page-btn">
-          Learn More and start today
-      </button>
-  </div>
-      <div class="socials">
-          <i class="fi-snsuxl-facebook"></i>
-          <i class="fi-xnsuxl-instagram"></i>
-          <i class="fi-xnsuxl-pinterest"></i>
-          <i class="fi-xnsuxl-twitter"></i>
-          <i class="fi-xnsuxl-linkedin"></i>
-          <i class="fi-cnsuxl-snapshat-solid"></i>
+        </p>
+        <button class="home-page-btn">Learn More and start today</button>
       </div>
-  
-</div>;
-
+      <div class="socials">
+        <i class="fi-snsuxl-facebook"></i>
+        <i class="fi-xnsuxl-instagram"></i>
+        <i class="fi-xnsuxl-pinterest"></i>
+        <i class="fi-xnsuxl-twitter"></i>
+        <i class="fi-xnsuxl-linkedin"></i>
+        <i class="fi-cnsuxl-snapshat-solid"></i>
+      </div>
+      <div class="background-container">
+        <img src="/mac.jpg" />
+      </div>
+    </div>
+  );
 }
 export default Home;
